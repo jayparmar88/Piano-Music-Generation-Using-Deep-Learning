6b2440fb1ac0a88e2bae654e465ae45004717f08
JAY <82171323+jayparmar88@users.noreply.github.com>
2023-04-29 00:52:31 +0530

    Update info_page.py

--- info_page.py
+++ info_page.py
@@ -4,36 +4,20 @@ def info():
 
     st.header("Model")
 
-    st.write("This model consists of 3-layer LSTM RNN network. It is used to capture\
-    the sequence of notes from the sample music chords provided to the model.\
-    We have tried catching patterns in the chords and notes, while maintaining the harmonic \
-    trend of the music track, and structuring the song in a fixed temporal structure\
-    We start with monophonic harmonies of a single popular instrument, in this\
-    case, a PIANO. We have a list of piano midi files that can be broken down to\
-    their chords and notes, and imply the char-RNN architecture on them.\
-    We create a simple LSTM network that treats the midi sequences and generated\
-    similar patterns for musical piece.")
+    st.write("In our model we use four different types of layers:")
+    st.write("LSTM layers is a Recurrent Neural Net layer that takes a sequence as an input and can return either sequences (return_sequences=True) or a matrix.")
+    st.write("Dropout layers are a regularisation technique that consists of setting a fraction of input units to 0 at each update during the training to prevent \
+    overfitting. The fraction is determined by the parameter used with the layer.")
+    st.write("Dense layers or fully connected layers is a fully connected neural network layer where each input node is connected to each output node.")
+    st.write("The Activation layer determines what activation function our neural network will use to calculate the output of a node.")
 
+    st.write("For each LSTM, Dense, and Activation layer the first parameter is how many nodes the layer should have. For the Dropout \
+    layer the first parameter is the fraction of input units that should be dropped during training. For each LSTM, Dense, and \
+    Activation layer the first parameter is how many nodes the layer should have. For the Dropout layer the first parameter is the \
+    fraction of input units that should be dropped during training.")
+    
     st.subheader("📻 Model Architechture")
-    st.image("./src/assets/img/Model_Architechture.png", 
-            caption = "Model layer arch",
-            width = 500)
-
-    st.subheader("Future Scope and Access")
-    st.markdown("<ol>\
-                <li>Make a Multi-track Sequential Networks for Symbolic Music Generation and Accompaniment</li>\
-                <li>Generate coherent music of four bars right from scratch, without human inputs.</li>\
-                <li>Given a specific track composed by a human, generate additional tracks to accompany it</li>\
-                <li>Device a hybrid model for polyphonic music generation consisting of several instruments.</li>\
-                <li>Creating a lyrically based generator for complex musical patterns.</li>\
-                </ol>", unsafe_allow_html = True)
-
-
-
-
-
-
-
+    st.image("./src/assets/img/Model_Architechture.png", width = 600)
 
     st.markdown('---')
-    st.caption("made by Dhruvi Shah & Visalakshi Iyer @Kaizen2k22")    
+    st.caption("made by Jay Parmar")
