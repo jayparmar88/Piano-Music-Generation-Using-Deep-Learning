23d0a04000d16baa54b49a8cc1fba7b1fbaf8bbd
JAY <82171323+jayparmar88@users.noreply.github.com>
2023-04-29 01:11:55 +0530

    Update convert_midi.py

--- convert_midi.py
+++ convert_midi.py
@@ -8,7 +8,7 @@ def convert_midi_to_wav(midifile):
     audio_data = midi_data.fluidsynth()
     audio_data = np.int16(
         audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
-    )  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
+    )
 
     virtualfile = io.BytesIO()
     wavfile.write(virtualfile, 44100, audio_data)
