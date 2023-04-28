550179f675446492ecf57cc4f8d492d6aae9ee86
JAY <82171323+jayparmar88@users.noreply.github.com>
2023-04-29 00:27:45 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -14,12 +14,13 @@ def abstract():
     st.subheader("Input music data")
     st.write("For generating music, first we have to train model using input music data. for \
     the input data we are passing midi files. The data splits into two  object types: notes and chords. \
-    Note objects contain information about the pitch, octave, and offset of the Note. \
-    ● Pitch refers to the frequency of the sound, or how high or low it is and is represented with the letters [A, B, C, D, E, F, G], with A being \
-    the highest and G being the lowest. \
-    ● Octave refers to which set of pitches you use on a piano. \
-    ● Offset refers to where the note is located in the piece. \
-    And Chord objects are essentially a container for a set of notes that are played at the same time.")
+    Note objects contain information about the pitch, octave, and offset of the Note. \")
+    
+    st.write("● Pitch refers to the frequency of the sound, or how high or low it is and is represented with the letters [A, B, C, D, E, F, G], with A being \
+    the highest and G being the lowest.")
+    st.write("● Octave refers to which set of pitches you use on a piano.")
+    st.write("● Offset refers to where the note is located in the piece.")
+    st.write("And Chord objects are essentially a container for a set of notes that are played at the same time.")
     
     st.markdown('---')
     st.caption("made by Jay Parmar")
