a938cc4f81218d2572f72f8161ef1d3713865bef
JAY <82171323+jayparmar88@users.noreply.github.com>
2023-05-01 12:08:23 +0530

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -110,7 +110,7 @@ def create_network(network_input, n_vocab):
 
 def train(model, network_input, network_output):
     """ train the neural network """
-    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    filepath = "weights.hdf5"
     checkpoint = ModelCheckpoint(
         filepath,
         monitor='loss',
